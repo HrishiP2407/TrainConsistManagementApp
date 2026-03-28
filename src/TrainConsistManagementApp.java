6f46cf59cccbb11dac7bc353f9f428a09937033b
HrishiP2407 <hrishipenti24072006@gmail.com>
2026-03-28 23:14:56 +0530

    UC7: Sort bogies using Comparator

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,20 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String toString() {
+        return name + " - " + capacity;
+    }
+}
 
 public class TrainConsistManagementApp {
 
@@ -7,14 +22,17 @@ public class TrainConsistManagementApp {
 
         System.out.println("=== Train Consist Management App ===");
 
-        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        List<Bogie> bogies = new ArrayList<>();
 
-        bogieCapacity.put("Sleeper", 72);
-        bogieCapacity.put("AC Chair", 60);
-        bogieCapacity.put("First Class", 40);
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 60));
+        bogies.add(new Bogie("First Class", 40));
 
-        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        bogies.sort(Comparator.comparingInt(b -> b.capacity));
+
+        System.out.println("Sorted Bogies by Capacity:");
+        for (Bogie b : bogies) {
+            System.out.println(b);
         }
     }
 }
